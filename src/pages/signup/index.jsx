fb57229c2310fa0c7970f477267d4ef34dbb1c3f
adiofficial04 <patiladinath560@gmail.com>
2025-04-01 15:02:04 +0530

    added title

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,5 +1,6 @@
 export default function signup(){
     return (
-        <div><h1 className="text-center text-blue-300 bg-grey-300">The Signup Page</h1></div>
+        <div><h1 className="text-center text-blue-300 bg-grey-300">The Signup Page</h1>
+        <h2 className="text-4xl text-red-600">Hackathon 2k25 ... We are goint to attend </h2></div>
     );
 }
