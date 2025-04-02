b4239502d956c70ac1618eaf4c2569d96e6f5119
adiofficial04 <patiladinath560@gmail.com>
2025-04-03 01:26:03 +0530

    adi al set

--- src/component/layouts/footer.jsx
+++ src/component/layouts/footer.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -10,7 +10,7 @@ function Footer() {
           href={"/"}
           className="flex title-font font-extrabold items-center  uppercase text-gray-100"
         >
-          <Image alt="Navbar Logo" src={"/Pizza.svg"} width={60} height={60} />
+          {/* <Image alt="Navbar Logo" src={"/Pizza.svg"} width={60} height={60} /> */}
           <p className="leading-5 text-xl mx-2">Pizza Wizza</p>
         </Link>
         <p className="text-sm text-gray-100 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
